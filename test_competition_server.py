c9d29beb69a36707aad5e63c446fd8dba5a22e99
chingxi <chengchingxi@gmail.com>
2024-06-01 16:39:04 +0800

    deleted:    .env.example modified:   test_competition_server.py

--- test_competition_server.py
+++ test_competition_server.py
@@ -7,9 +7,97 @@ from typing import Any, Dict, List
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
-from scoring.nlp_eval import nlp_eval
-from scoring.vlm_eval import vlm_eval
 from starlette.responses import FileResponse
+
+#From nlp_eval.py:
+from statistics import mean
+from rouge_score import rouge_scorer
+from typing import List, Dict
+
+ROUGEL = "rougeL"
+scorer = rouge_scorer.RougeScorer([ROUGEL], use_stemmer=True)
+
+keys = ("heading", "target", "tool")
+
+
+def score(key: str, ref: str, hyp: str) -> float:
+    # exact match for heading
+    if key == "heading":
+        return 1.0 if ref == hyp else 0.0
+    # ROUGE-L for everything else
+    else:
+        score = scorer.score(ref, hyp)[ROUGEL]
+        return score.fmeasure
+
+
+def nlp_eval(truth: List[Dict[str, str]], hypothesis: List[Dict[str, str]]) -> float:
+    results = []
+    for ref, hyp in zip(truth, hypothesis):
+        results.append(mean(score(key, ref[key], hyp[key]) for key in keys))
+    return mean(results)
+
+
+
+
+#From vlm_eval.py:
+from statistics import mean
+from typing import List, Optional
+
+
+def bb_iou(bb1: Optional[List[int]], bb2: List[int]) -> int:
+    """
+    Calculate the Intersection over Union (IoU) of two bounding boxes in ltwh format.
+
+    Parameters
+    ----------
+    bb1 : list[int, int, int, int] | None
+        left, top, width, height
+    bb2 : list[int, int, int, int]
+        left, top, width, height
+
+    Returns
+    -------
+    int
+        0 or 1
+    """
+    if not bb1:
+        return 0
+    boxA = [bb1[0], bb1[1], bb1[0] + bb1[2], bb1[1] + bb1[3]]
+    boxB = [bb2[0], bb2[1], bb2[0] + bb2[2], bb2[1] + bb2[3]]
+
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = abs(max((xB - xA, 0)) * max((yB - yA), 0))
+    if interArea == 0:
+        return 0.0
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = abs((boxA[2] - boxA[0]) * (boxA[3] - boxA[1]))
+    boxBArea = abs((boxB[2] - boxB[0]) * (boxB[3] - boxB[1]))
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value @ 0.5
+    return round(iou)
+
+
+def vlm_eval(
+    bbox_truths: List[Optional[List[int]]], bbox_predictions: List[List[int]]
+) -> float:
+    return mean(
+        bb_iou(bb_truth, bb_pred)
+        for bb_truth, bb_pred in zip(bbox_truths, bbox_predictions)
+    )
+
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
